fc70abcc8e144c39773ab8cef906efd13c7aa7e2
Kevin Knopp (V) <kknopp@apple.com>
2026-04-15 13:53:04 -0500

    setting up netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+
+  devToolbar: {
+    enabled: false
+  },
+
+  adapter: netlify()
 });
